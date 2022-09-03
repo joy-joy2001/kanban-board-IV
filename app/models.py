bc3f33e4215ddbbd351048691554570df420dc06
joy-joy2001 <jefiannayi@gmail.com>
2022-09-03 12:18:56 +0100

    create db

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.VARCHAR(255), nullable=False)
     date_created = db.Column(db.String(50), nullable=False)
     tasks = db.relationship('Task', backref='user', lazy=True)
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(80), nullable=False)
+    type = db.Column(db.String(255), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     priority = db.Column(db.String(50), nullable=False)
     due_date = db.Column(db.String(50), nullable=False)
