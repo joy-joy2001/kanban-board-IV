b8b919f57487cc28d0290ee412cba2d2c18703ee
joy-joy2001 <jefiannayi@gmail.com>
2022-09-03 11:41:23 +0100

    string -> varchar(255)

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password = db.Column(db.VARCHAR(255), nullable=False)
     date_created = db.Column(db.String(50), nullable=False)
     tasks = db.relationship('Task', backref='user', lazy=True)
 
