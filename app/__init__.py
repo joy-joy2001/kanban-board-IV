d5ac8bd5c06ed970f80807216ba0e4e82881ed0b
Joy <66254898+joy-joy2001@users.noreply.github.com>
2022-09-11 08:40:40 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ my_app.config['RECAPTCHA_PRIVATE_KEY'] = os.environ.get('RECAPTCHA_SECRET_KEY')
 my_app.config['RECAPTCHA_OPTIONS'] = {'data-theme': 'dark'}
 
 # this will link the flask app to the database.
-db.init_app(my_app)
+# db.init_app(my_app)
 
 
 ### from the python package named 'app', the 'views.py' model is being imported.
